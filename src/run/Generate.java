eae5cadf723cd5d59e69490dd3a69fd0fd5408ef
Mananjara1754 <mananjara888@gmail.com>
2023-12-05 14:16:06 +0300

    First commit

---
+++ src/run/Generate.java
@@ -0,0 +1,17 @@
+package run;
+
+import scaffolding.Scaffold;
+
+public class Generate {
+    public static void main(String[] args) {
+        try {
+            System.out.println("generating...");
+            //Scaffold.generate("nba", "joueur", "joueur","t","java");
+            Scaffold.generate("commerce", "service", "service","t","java");
+            //Scaffold.generate(args[0], args[1], args[2],args[3],"csharp");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
